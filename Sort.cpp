f21479591599d6607444f0cfcf0084c407b38942
Parth Khajgiwale <90266917+parthkhajgiwale@users.noreply.github.com>
2025-04-28 14:23:59 +0530

    Update Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -2,7 +2,7 @@
 #include <omp.h>
 using namespace std;
 
-// ===== MERGE SORT FUNCTIONS =====
+// ===== MERGE FUNCTION =====
 void merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
@@ -16,7 +16,7 @@ void merge(int arr[], int left, int mid, int right) {
         R[j] = arr[mid + 1 + j];
 
     int i = 0, j = 0, k = left;
-    
+
     while (i < n1 && j < n2)
         arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
 
@@ -27,25 +27,56 @@ void merge(int arr[], int left, int mid, int right) {
     delete[] R;
 }
 
-void mergeSort(int arr[], int left, int right) {
+// ===== Traditional Merge Sort =====
+void mergeSortTraditional(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSortTraditional(arr, left, mid);
+        mergeSortTraditional(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+// ===== Parallel OMP Merge Sort =====
+void mergeSortOMP(int arr[], int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
 
         #pragma omp parallel sections
         {
             #pragma omp section
-            mergeSort(arr, left, mid);
+            mergeSortOMP(arr, left, mid);
 
             #pragma omp section
-            mergeSort(arr, mid + 1, right);
+            mergeSortOMP(arr, mid + 1, right);
         }
 
         merge(arr, left, mid, right);
     }
 }
 
-// ===== BUBBLE SORT FUNCTION =====
-void bubbleSort(int arr[], int n) {
+// ===== Traditional Bubble Sort =====
+void bubbleSortTraditional(int arr[], int n) {
+    bool swapped;
+
+    for (int i = 0; i < n - 1; ++i) {
+        swapped = false;
+
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+}
+
+// ===== Parallel OMP Bubble Sort =====
+void bubbleSortOMP(int arr[], int n) {
     bool swapped;
 
     for (int i = 0; i < n - 1; ++i) {
@@ -54,10 +85,7 @@ void bubbleSort(int arr[], int n) {
         #pragma omp parallel for shared(arr, swapped)
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
-                // Swap
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
@@ -74,10 +102,10 @@ int main() {
     cout << "Enter number of elements: ";
     cin >> n;
 
-    int* arr = new int[n];
+    int* original = new int[n];
     cout << "Enter " << n << " elements:\n";
     for (int i = 0; i < n; ++i)
-        cin >> arr[i];
+        cin >> original[i];
 
     cout << "\nChoose sorting algorithm:\n";
     cout << "1. Merge Sort\n";
@@ -85,31 +113,66 @@ int main() {
     cout << "Enter choice: ";
     cin >> choice;
 
+    double startTime, endTime;
+
+    // Copy arrays to work separately
+    int* arrTraditional = new int[n];
+    int* arrOMP = new int[n];
+
+    for (int i = 0; i < n; ++i) {
+        arrTraditional[i] = original[i];
+        arrOMP[i] = original[i];
+    }
+
     switch (choice) {
         case 1:
+            cout << "\n=== Traditional Merge Sort ===\n";
+            startTime = omp_get_wtime();
+            mergeSortTraditional(arrTraditional, 0, n - 1);
+            endTime = omp_get_wtime();
+            cout << "Execution Time (Traditional Merge Sort): " << (endTime - startTime) << " seconds\n";
+
+            cout << "\n=== OMP Parallel Merge Sort ===\n";
+            startTime = omp_get_wtime();
             #pragma omp parallel
             {
                 #pragma omp single
-                mergeSort(arr, 0, n - 1);
+                mergeSortOMP(arrOMP, 0, n - 1);
             }
-            cout << "\nArray sorted using Merge Sort:\n";
+            endTime = omp_get_wtime();
+            cout << "Execution Time (OMP Parallel Merge Sort): " << (endTime - startTime) << " seconds\n";
             break;
 
         case 2:
-            bubbleSort(arr, n);
-            cout << "\nArray sorted using Bubble Sort:\n";
+            cout << "\n=== Traditional Bubble Sort ===\n";
+            startTime = omp_get_wtime();
+            bubbleSortTraditional(arrTraditional, n);
+            endTime = omp_get_wtime();
+            cout << "Execution Time (Traditional Bubble Sort): " << (endTime - startTime) << " seconds\n";
+
+            cout << "\n=== OMP Parallel Bubble Sort ===\n";
+            startTime = omp_get_wtime();
+            bubbleSortOMP(arrOMP, n);
+            endTime = omp_get_wtime();
+            cout << "Execution Time (OMP Parallel Bubble Sort): " << (endTime - startTime) << " seconds\n";
             break;
 
         default:
             cout << "Invalid choice!\n";
-            delete[] arr;
+            delete[] original;
+            delete[] arrTraditional;
+            delete[] arrOMP;
             return 1;
     }
 
+    cout << "\n=== Final Sorted Array (OMP Version) ===\n";
     for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
+        cout << arrOMP[i] << " ";
     cout << endl;
 
-    delete[] arr;
+    delete[] original;
+    delete[] arrTraditional;
+    delete[] arrOMP;
+
     return 0;
 }
